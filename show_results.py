98539f6aa833f6766cb044f633cf95635d6cce58
itsmenisha <90882851+itsmenisha@users.noreply.github.com>
2025-10-07 08:43:38 +0545

    Update and rename export_tiny.py to show_results.py

---
+++ show_results.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+model = YOLO(r"runs/detect/exp_cpu_fast2/weights/best.pt")
+
+results = model.predict(
+    source=r"C:\Users\Acer\Desktop\projects\Real-Time Traffic Sign Recognition\traffic sign test video.mp4",
+    imgsz=5000,      
+    conf=0.15,      
+    iou=0.45,      
+    show=True,       
+    save=True,  
+    project="runs/detect",
+    name="video_test",
+    stream=False
+)
